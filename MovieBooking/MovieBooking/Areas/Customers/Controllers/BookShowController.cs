e85bf50f07c8b8a22f0b02b8efb970d2470af850
Kamal Singh <kamalu960@gmail.com>
2018-05-27 12:02:21 +0530

    Movie Booking Project added
    Movie Booking Project

---
+++ MovieBooking/MovieBooking/Areas/Customers/Controllers/BookShowController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieBooking.Areas.Customers.Controllers
+{
+    public class BookShowController : Controller
+    {
+        //
+        // GET: /Customers/BookShow/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
